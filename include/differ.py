dc7156694b930b73758f2c5a1a259d81afe2ea23
KunWuLuan <kunwu1997@qq.com>
2021-12-20 20:08:35 +0800

    it can profile by batch

--- include/differ.py
+++ include/differ.py
@@ -1,14 +1,17 @@
-from pytorchyolo.utils.utils import get_batch_statistics, ap_per_class
+from pytorchyolo.utils.utils import get_batch_statistics, compute_ap
 from pytorchyolo.test import print_eval_stats
 import torch
 import numpy as np
 from . import cococlasses
 
 iou_thres = 0.7
-verbose = True
+verbose = False
 
 def feature_diff(golden, o):
-    pass
+    similarity = torch.zeros(len(golden))
+    for i in range(len(golden)):
+        similarity[i] = torch.cosine_similarity(golden[i].reshape(1,-1), o[i].reshape(1,-1))
+    return similarity
 
 def outputs2targets(golden):
     targets = torch.zeros(len(golden), 6)
@@ -27,18 +30,76 @@ def output_diff(golden, o):
     targets = outputs2targets(golden)
     labels += targets[:, 1].tolist()
     sample_metrics += get_batch_statistics([o], targets, iou_threshold=iou_thres)
-    # print(targets)
 
     if len(sample_metrics) == 0:  # No detections over whole validation set.
         print("---- No detections over whole validation set ----")
         return None
 
-        # Concatenate sample statistics
-    true_positives, pred_scores, pred_labels = [
-        np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
-    metrics_output = ap_per_class(
-        true_positives, pred_scores, pred_labels, labels)
+    if len(labels) == 0:
+        return {}
+    # Concatenate sample statistics
+    true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
+    metrics_output = ap_per_class(true_positives, pred_scores, pred_labels, labels)
 
-    print_eval_stats(metrics_output, cococlasses.classes, verbose)
+    metrics_dict = {}
+    for i, cls in enumerate(metrics_output[-1]):
+        metrics_dict[cls] = torch.Tensor([metrics_output[0][i],metrics_output[1][i],metrics_output[2][i],metrics_output[3][i]])
 
-    return metrics_output
+    return metrics_dict
+
+def ap_per_class(tp, conf, pred_cls, target_cls):
+    """ Compute the average precision, given the recall and precision curves.
+    Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
+    # Arguments
+        tp:    True positives (list).
+        conf:  Objectness value from 0-1 (list).
+        pred_cls: Predicted object classes (list).
+        target_cls: True object classes (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+
+    该函数和pytorchyolo的完全一样，只是除去了进度条
+    """
+
+    # Sort by objectness
+    i = np.argsort(-conf)
+    tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
+
+    # Find unique classes
+    unique_classes = np.unique(target_cls)
+
+    # Create Precision-Recall curve and compute AP for each class
+    ap, p, r = [], [], []
+    for c in unique_classes:
+        i = pred_cls == c
+        n_gt = (target_cls == c).sum()  # Number of ground truth objects
+        n_p = i.sum()  # Number of predicted objects
+
+        if n_p == 0 and n_gt == 0:
+            continue
+        elif n_p == 0 or n_gt == 0:
+            ap.append(0)
+            r.append(0)
+            p.append(0)
+        else:
+            # Accumulate FPs and TPs
+            fpc = (1 - tp[i]).cumsum()
+            tpc = (tp[i]).cumsum()
+
+            # Recall
+            recall_curve = tpc / (n_gt + 1e-16)
+            r.append(recall_curve[-1])
+
+            # Precision
+            precision_curve = tpc / (tpc + fpc)
+            p.append(precision_curve[-1])
+
+            # AP from recall-precision curve
+            ap.append(compute_ap(recall_curve, precision_curve))
+
+    # Compute F1 score (harmonic mean of precision and recall)
+    p, r, ap = np.array(p), np.array(r), np.array(ap)
+    f1 = 2 * p * r / (p + r + 1e-16)
+
+    return p, r, ap, f1, unique_classes.astype("int32")
+
--- profilesystem.py
+++ profilesystem.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 import torch
 import include.mymodels as mymodels
 import include.extractors as extractors
@@ -16,6 +17,53 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def single_frame_profile(reader, controllers):
+    batch_profile(reader, controllers, batch_size=1)
+
+def batch_profile(reader, controllers, batch_size, max_batch = -1):
+    print('profile start...')
+    start_time = time.time_ns()
+    batch_count = 0
+    ret, next_frame = reader.next()
+    while ret == 1 and (max_batch == -1 or batch_count < max_batch):
+        count = 0
+        batch_start_time = time.time_ns()
+        batch_feature_metrics = [None for i in range(len(controllers)-1)]
+        batch_output_metrics = [{} for i in range(len(controllers)-1)]
+        print('batch {} start...'.format(batch_count))
+        while count < batch_size:
+            golden_feature = None
+            golden_output = None
+            for i, c in enumerate(controllers):
+                cur_feature, cur_output = c.execute_once(next_frame)
+                if golden_feature is None:
+                    golden_feature = cur_feature
+                    golden_output = cur_output
+                else:
+                    feature_output = feature_diff(golden_feature, cur_feature)
+                    if batch_feature_metrics[i-1] is None:
+                        batch_feature_metrics[i-1] = feature_output
+                    else:
+                        batch_feature_metrics[i-1] += feature_output
+
+                    metrics_output = output_diff(golden_output, cur_output)
+                    for k,v in metrics_output.items():
+                        v = v.unsqueeze(0)
+                        if k in batch_output_metrics[i-1].keys():
+                            batch_output_metrics[i-1][k] = torch.cat((batch_output_metrics[i-1][k],v), dim=0)
+                        else:
+                            batch_output_metrics[i-1][k] = v
+            count = count+1
+            ret, next_frame = reader.next()
+        print('batch {} end. cost {}s ...'.format(batch_count, (time.time_ns()-batch_start_time)/1000000000))
+        for i in range(len(batch_output_metrics)):
+            for k,v in batch_output_metrics[i].items():
+                print('class {}, mean:{}'.format(k, torch.mean(batch_output_metrics[i][k], dim=0)))
+            print('tensor of different layers: {}'.format(batch_feature_metrics[i].div(batch_size)))
+        batch_count = batch_count + 1
+    print('end.\n cost time: {}s'.format((time.time_ns()-start_time)/1000000000))
+
+
 if __name__ == '__main__':
     print('system start...')
     args = parse_args()
@@ -29,26 +77,4 @@ if __name__ == '__main__':
 
     controllers = build_controllers(args.net, extractor, device)
     reader = VideoReader(args.path)
-    ret, next_frame = reader.next()
-    print('detect start...')
-    start_time = time.time_ns()
-    count = 0
-    max_count = 10
-    while ret == 1 and (max_count == -1 or count < max_count):
-        golden_feature = None
-        golden_output = None
-        cur_features = []
-        cur_outputs = []
-        for c in controllers:
-            cur_feature, cur_output = c.execute_once(next_frame)
-            # print(cur_output)
-            if golden_feature is None:
-                golden_feature = cur_feature
-                golden_output = cur_output
-            else:
-                cur_features.append(cur_feature)
-                cur_outputs.append(cur_output)
-                metrics = output_diff(golden_output, cur_output)
-        count = count+1
-        ret, next_frame = reader.next()
-    print('end.\n cost time: {}s'.format((time.time_ns()-start_time)/1000000000))
+    batch_profile(reader=reader, controllers=controllers, batch_size=30, max_batch=2)
